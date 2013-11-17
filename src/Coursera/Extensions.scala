46898ad02ddf605166ebdf10ff3a6e0e9769aa58
headinthebox <emeijer@applied-duality.com>
2013-11-16 16:27:22 -0800

    Small fixes

--- src/Coursera/Extensions.scala
+++ src/Coursera/Extensions.scala
@@ -6,23 +6,25 @@ import scala.util.{Failure, Success, Try}
 package object Extensions {
 
   implicit class ListExtensions[T](val source: List[T]) extends AnyVal {
-    def sumBy[B](keySelector: T => Integer): Integer = ???
+    def sumBy(keySelector: T => Integer): Integer = ???
 
-    def zumBy[B](keySelector: T => B)(implicit num: Numeric[B]): B = {
+    private def sumBy[B](keySelector: T => B)(implicit num: Numeric[B]): B = {
         source.map(keySelector).sum
     }
 
   }
 
   def f[T](that: Future[T]): PartialFunction[Throwable, Future[T]] = { case _: Throwable => that }
+  def g[T]: PartialFunction[Throwable, Failure[T]] = { case t: Throwable => Failure(t) }
 
   implicit class FutureExtensions[T](val future: Future[T]) extends AnyVal {
+
     def fallbackTo[U >: T](that: =>Future[U])(implicit executor: ExecutionContext): Future[U] = {
       future.recoverWith(f(that.recoverWith(f(future))))
     }
 
-    def withTry(): Future[Try[T]] = {
-      future.map(Success(_)) recover { case t: Throwable => Failure(t) }
+    def withTry()(implicit executor: ExecutionContext): Future[Try[T]] = {
+      future.map(Success(_)) recover g
     }
   }
 
