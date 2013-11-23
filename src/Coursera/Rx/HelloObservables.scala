969523e7ca1c824895b7631c8673de1e64a93261
headinthebox <emeijer@applied-duality.com>
2013-11-23 15:36:45 -0800

    Added flatten example

--- src/Coursera/Rx/HelloObservables.scala
+++ src/Coursera/Rx/HelloObservables.scala
@@ -1,6 +1,6 @@
 package coursera.rx
 
-import rx.lang.scala.{Subscription, Observable}
+import rx.lang.scala.{Observable, Subscription}
 import scala.languageFeature.postfixOps._
 import scala.concurrent.duration._
 
@@ -19,6 +19,20 @@ object HelloObservables {
 
     // stop the stream
     subscription.unsubscribe()
+  }
+
+}
+
+object Nested {
+
+  def flattenNestedStreams(): Unit = {
+
+    val xs: Observable[Int]              = Observable(3,2,1)
+    val yss: Observable[Observable[Int]] = xs.map(x => Observable.interval(x seconds).map(_ => x).take(2))
+    val zs = yss.flatten
+
+    println(zs.toBlockingObservable.toList.equals(List(1,2,1,3,2,3)))
+
   }
 
 }
