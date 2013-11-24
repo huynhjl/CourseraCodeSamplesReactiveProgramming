524adcfb8da2bd7a6241d92909959fe52ab22dff
headinthebox <emeijer@applied-duality.com>
2013-11-23 18:10:20 -0800

    Reverse Geocode

--- src/Coursera/usgs/Point.scala
+++ src/Coursera/usgs/Point.scala
@@ -3,10 +3,10 @@ package coursera.usgs
 class Point {
   private val coordinates: Array[Double] = null
 
-  lazy val Latitude: Double              = coordinates(1)
-  lazy val Longitude  : Double           = coordinates(0)
-  lazy val Altitude  : Double            = coordinates(2)
+  lazy val latitude: Double              = coordinates(1)
+  lazy val longitude  : Double           = coordinates(0)
+  lazy val altitude  : Double            = coordinates(2)
 
-  override def toString() = s"{ 'longitude':'${Longitude}', 'latitude':'${Latitude}', 'altitude':'${Altitude}' }";
+  override def toString() = s"{ 'longitude':'${longitude}', 'latitude':'${latitude}', 'altitude':'${altitude}' }";
 
 }
