56fd3b8059ac40b44dd737f661e6fab58e7850c5
headinthebox <emeijer@applied-duality.com>
2013-11-23 20:09:18 -0800

    Earthquakes with country

--- src/Main.scala
+++ src/Main.scala
@@ -11,6 +11,8 @@ object Main {
     // EarthQuakes.quakes().subscribe(println(_))
     // EarthQuakes.major().subscribe(println(_))
     // EarthQuakes.ofMagnitude(Magnitude.Minor).subscribe(println(_))
+    // EarthQuakes.withCountryMerged().subscribe(println(_), e => println(e.getMessage), () => println(">>>>>>"))
+    // EarthQuakes.withCountryConcatenated().subscribe(println(_), e => println(e.getMessage), () => println(">>>>>>"))
   }
 
 }
